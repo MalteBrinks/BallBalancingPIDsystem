7a6693b37689353e3aa094a3b4b35eb321ea05ce
MalteBrinks <74729626+MalteBrinks@users.noreply.github.com>
2023-05-16 18:15:33 +0200

    Update main.py
    shortened Code

--- main.py
+++ main.py
@@ -18,10 +18,8 @@ yellow = Motor C
 """
 redRange = np.array([6,81,130]), np.array([41,255,233])
 
-pid = 11, .96, 204   #15, .96, 204         11, .96, 204 
-greenPIDval = pid
-bluePIDval = pid
-yellowPIDval = pid
+pid = (11, .96, 204)
+greenPIDval = bluePIDval = yellowPIDval = pid
 
 camera = PiCamera()
 camera.resolution = (640, 480)
@@ -54,9 +52,13 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         ballY, ballX = averagePixelPos(ballPos)
         
         if not (math.isnan(ballX) or math.isnan(ballY)):
-            greenDistance = calcDistance((ballX, ballY), greenPos)
-            blueDistance = calcDistance((ballX, ballY), bluePos)
-            yellowDistance = calcDistance((ballX, ballY), yellowPos)
+            positions = [greenPos, bluePos, yellowPos]
+            distances = []
+            for pos in positions:
+                distance = calcDistance((ballX, ballY), pos)
+                distances.append(distance)
+
+            greenDistance, blueDistance, yellowDistance = distances
             
             oldGreenError = greenError
             oldBlueError = blueError
@@ -76,26 +78,17 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
             blueRotation = mapToServo(bluePID)
             yellowRotation = mapToServo(yellowPID)
             
-            setServo(PWM, greenPin, greenRotation)
-            setServo(PWM, bluePin, blueRotation)
-            setServo(PWM, yellowPin, yellowRotation)
+            for pin in [greenPin, bluePin, yellowPin]:
+                setServo(PWM, pin, greenRotation)
         else:
             
             setServo(PWM, greenPin, 500)
             setServo(PWM, bluePin, 2500)
             setServo(PWM, yellowPin, 500)
 
-        
-        cv2.circle(frame, greenPos, int(wantedDisGreen), (255,0,0))
-        cv2.circle(frame, bluePos, int(wantedDisBlue), (255,0,0))
-        cv2.circle(frame, yellowPos, int(wantedDisYellow), (255,0,0))
-        cv2.circle(frame, greenPos, 10, (255,0,0))
-        cv2.circle(frame, bluePos, 10, (255,0,0))
-        cv2.circle(frame, yellowPos, 10, (255,0,0))
-        
-        #cv2.imshow("mask", mask)
-        #cv2.imshow("frame", frame)
-        #cv2.imwrite("/home/malte/frame.jpeg", frame)
+        for pos in [greenPos, bluePos, yellowPos]:
+            cv2.circle(frame, pos, int(wantedDisGreen), (255,0,0))
+            cv2.circle(frame, pos, 10, (255,0,0))
 
         key = cv2.waitKey(1) & 0xFF
         rawCapture.truncate(0)
@@ -103,9 +96,9 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
             break
     except:
         break
-killServo(PWM, greenPin)
-killServo(PWM, bluePin)
-killServo(PWM, yellowPin)
+
+for pin in [greenPin, bluePin, yellowPin]:
+    killServo(PWM, pin)
 
 cv2.destroyAllWindows()
 
